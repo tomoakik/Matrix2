1b119b9382a5f36c621c7881aed941325787857b
50616013 <50616013@50616013-pc>
2017-11-11 00:10:02 +0900

    matrix2

---
+++ src/matrix2/Matrix2_main.java
@@ -0,0 +1,20 @@
+package matrix2;
+
+import java.util.Arrays;
+
+public class Matrix2_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double a[][] = {{1,2},{-1,1},};
+		double b[][] = {{3,-1},{2,-1}};
+		double c[][] = {{3,1,1,2},{5,1,3,4},{2,0,1,0},{1,3,2,1}};
+		Matrix2_lib mlib = new Matrix2_lib();
+		System.out.println("Product = "+Arrays.deepToString(mlib.getProduct(a,b)));
+		System.out.println("Cofactor = "+(mlib.getCofactor(c)));
+		System.out.println("Inverse ="+Arrays.deepToString(mlib.getInverse(c)));
+
+	}
+
+
+}
